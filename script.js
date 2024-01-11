d4d6370fb9baa270d33b7bd88632b122f0c4d8b3
LADDA28 <150544946+ladda28@users.noreply.github.com>
2024-01-12 00:14:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('showProfileBtn').addEventListener('click', function() {
+    var profileContainer = document.getElementById('profileContainer');
+    profileContainer.classList.remove('hidden');
+});
